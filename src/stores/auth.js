1f004a7587d6be08df98115b7c6975f06ebb6806
Saeed <saeed17@gmail.com>
2023-04-22 22:52:50 +0100

    Auth store

---
+++ src/stores/auth.js
@@ -0,0 +1,28 @@
+import { defineStore } from 'pinia'
+import AuthService from '@/services/AuthService'
+import router from '@/router'
+
+export const useAuthStore = defineStore({
+  id: 'auth',
+  state: () => ({
+    loggedInUser: null,
+  }),
+  getters: {},
+  actions: {
+    async getAuthenticatedUserDetails() {
+      try {
+        const response = await AuthService.getAuthUser()
+        this.loggedInUser = response.data.data
+      } catch (error) {
+        console.error(error)
+        this.loggedInUser = null
+      }
+    },
+    logout() {
+      return AuthService.logout().then(() => {
+        this.loggedInUser = null
+        if (router.currentRoute.name !== 'login') router.push({ name: 'login' })
+      })
+    },
+  },
+})
