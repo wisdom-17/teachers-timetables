3c1f14a01eaa61b573d7ed91abfae779a5f09b30
Saeed <saeed17@gmail.com>
2023-04-25 00:29:13 +0100

    Check for authenticated user when loading requiresAuth routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
+import { useAuthStore } from '@/stores/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_API_URL),
@@ -19,5 +20,28 @@ const router = createRouter({
     },
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  const storeAuth = useAuthStore()
+  const authUser = storeAuth.loggedInUser
+  const reqAuth = to.matched.some((record) => record.meta.requiresAuth)
+  const hideForAuth = to.matched.some((record) => record.meta.hideForAuth)
+  const loginQuery = { path: '/login', query: { redirect: to.fullPath } }
+
+  if (reqAuth && !authUser) {
+    storeAuth.getAuthenticatedUserDetails().then(() => {
+      if (!storeAuth.loggedInUser) next(loginQuery)
+      else next()
+    })
+  } else if (!authUser && hideForAuth) {
+    storeAuth.getAuthenticatedUserDetails().then(() => {
+      if (!storeAuth.loggedInUser) next()
+      else return next(from)
+    })
+  } else {
+    next() // make sure to always call next()!
+  }
+})
+
 
 export default router
