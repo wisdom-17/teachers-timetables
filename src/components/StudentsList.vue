6c2ec144a22a7e10e734546b0070ff36d56e0e57
Saeed <saeed17@gmail.com>
2023-04-27 01:07:53 +0100

    Student list component

---
+++ src/components/StudentsList.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <li v-for="student in students" :key="student">
+      {{ student }}
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useTimetableStore } from '@/stores/timetable'
+
+const storeTimetable = useTimetableStore()
+
+const students = computed(() => {
+  return storeTimetable.timetable[props.selectedClassStartDate][props.selectedLessonIndex]['students'].map(studentObj => {
+    return studentObj.forename + ' ' + studentObj.surname
+  }).sort()
+})
+
+const props = defineProps({
+  selectedClassStartDate : {
+    type: String,
+    required: true
+  },
+  selectedLessonIndex : {
+    type: String,
+    required: true
+  }
+})
+
+</script>
