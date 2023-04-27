62a0e32f336487be94d7c5ebf00513358079b2cb
Saeed <saeed17@gmail.com>
2023-04-27 01:05:35 +0100

    Pinia store for timetable

---
+++ src/stores/timetable.js
@@ -0,0 +1,28 @@
+import { defineStore } from 'pinia'
+import TimetableService from '@/services/Timetable'
+import router from '@/router'
+
+export const useTimetableStore = defineStore({
+  id: 'timetable',
+  state: () => ({
+    timetable: null,
+    isLoading: false,
+    employeeID: 'A500460806',
+  }),
+  getters: {},
+  actions: {
+    async getTimetable() {
+      this.isLoading = true
+      TimetableService.list(this.employeeID)
+        .then((data) => {
+          this.isLoading = false
+          this.timetable = data.data.timetable
+          router.push({ name: 'timetable' })
+        })
+        .catch((error) => {
+          this.isLoading = false
+          console.log(error)
+        })
+    },
+  },
+})
