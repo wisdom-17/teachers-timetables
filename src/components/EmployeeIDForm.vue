364702556ef3052bd65297fe56205f2a8618a309
Saeed <saeed17@gmail.com>
2023-04-27 01:04:58 +0100

    Use Pinia store in employee ID form

--- src/components/EmployeeIDForm.vue
+++ src/components/EmployeeIDForm.vue
@@ -4,45 +4,28 @@
         <div class="field grid">
           <label for="employeeID" class="col-8 col-offset-2 text-2xl xl:col-6 lg:col-offset-0 xl:text-4xl">Employee ID</label>
           <div class="col-8 col-offset-2 lg:col-6 lg:col-offset-0">
-            <InputText id="employeeID" v-model="employeeID" class="p-inputtext-lg w-full" />
+            <InputText id="employeeID" v-model="storeTimetable.employeeID" class="p-inputtext-lg w-full" />
           </div>
         </div>
         <div class="field grid">
           <div class="col-8 col-offset-2 xl:col-6 xl:col-offset-6">
-            <Button class="w-full" label="Show Timetable" size="large" :loading="isLoading" @click="getEmployeeTimetable"/>
+            <Button class="w-full" label="Show Timetable" size="large" :loading="storeTimetable.isLoading" @click="onClickShowTimetable"/>
           </div>
         </div>
         
     </form>
-  </div>
-
-    
+  </div>    
 </template>
 
 <script setup>
-import { ref } from 'vue'
 import Button from 'primevue/button';
 import InputText from 'primevue/inputtext';
+import { useTimetableStore } from '@/stores/timetable'
 
-const employeeID = ref('')
-const isLoading = ref(false)
+const storeTimetable = useTimetableStore()
 
-const getEmployeeTimetable = async () => {
-  console.log('todo: getEmployeeTimetable')
-  // const payload = {
-  //   employeeID: employeeID.value,
-  // }
-
-  try {
-    isLoading.value = true
-
-    isLoading.value = false
-
-  } catch (error) {
-    isLoading.value = false
-
-    // showErrorMessage.value = true
-  }
+const onClickShowTimetable = () => {
+  storeTimetable.getTimetable()
 }
 
 </script>
